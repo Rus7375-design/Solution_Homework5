20a313dac8e360d2d33df51e079b0f3bd7241c1b
Rus7375-design <serikzhan.rustam@gmail.com>
2025-04-04 21:14:48 +0500

    first commit

---
+++ RealImage.java
@@ -0,0 +1,7 @@
+public class RealImage implements Image {
+    private String filename;
+
+    public RealImage(String filename) {
+        this.filename = filename;
+        loadImageFromDisk();
+    }
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+// File: Main.java
+public class Main {
+    public static void main(String[] args) {
+    }
+}
