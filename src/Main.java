c77baa704da84c6d9a74667952e9984f1f068d21
Rus7375-design <serikzhan.rustam@gmail.com>
2025-04-05 11:13:34 +0500

    третий коммит

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 // File: Main.java
 public class Main {
     public static void main(String[] args) {
+        Image image = new RealImage("house1.jpg");
+        image.display();
+        image.displayFullImage();
     }
 }
--- src/RealImage.java
+++ src/RealImage.java
@@ -1,7 +1,20 @@
 public class RealImage implements Image {
-    private String filename;
-
-    public RealImage(String filename) {
-        this.filename = filename;
-        loadImageFromDisk();
+    private String fileName;
+    public RealImage(String fileName) {
+        this.fileName = fileName;
+        loadImage();
     }
+    private void loadImage() {
+        System.out.println("[RealImage] Загружается полноразмерное изображение с диска: " + fileName);
+    }
+    @Override
+    public void display() {
+        System.out.println("[RealImage] Показывается миниатюра изображения: " + fileName);
+    }
+
+    // Показ полноразмерного изображения
+    @Override
+    public void displayFullImage() {
+        System.out.println("[RealImage] Показывается полноразмерное изображение: " + fileName);
+    }
+}
